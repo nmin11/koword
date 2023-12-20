667b9082e86a22f5804101ecc013ecb0d6d23282
nmin11 <nmin1124@gmail.com>
2023-12-21 07:34:07 +0900

    chore: move object of words into main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,21 @@
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import command.Sample
 import kotlinx.cli.ArgParser
+import model.WordBook
+
+private val objectMapper: ObjectMapper =
+  ObjectMapper()
+    .registerKotlinModule()
+    .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+    .findAndRegisterModules()
+
+private val json = checkNotNull(WordsProperty::class.java.getResource("META-INF/words.json")).readText()
+object WordsProperty {
+  val wordBook = objectMapper.readValue<WordBook>(json)
+}
 
 fun main(args: Array<String>) {
   KowordApplication().execute(args)
---
+++ src/main/kotlin/command/Sample.kt
@@ -0,0 +1,13 @@
+package command
+
+import WordsProperty.wordBook
+import kotlinx.cli.Subcommand
+
+class Sample() : Subcommand(name = "sample", actionDescription = "샘플입니다") {
+  override fun execute() {
+    println("샘플 명령어 작동")
+    wordBook.words.forEach {
+      println(it)
+    }
+  }
+}
